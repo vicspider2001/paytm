833c6291d26d7543d0b369525451a5befb6e75a6
vicspider2001 <anogwihvictor@gmail.com>
2022-01-25 00:46:34 +0100

    update

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ app.post("/callback", (req, res) => {
          post_res.on('end', function(){
            console.log('S2S Response: ', response, "\n");
            var _results = JSON.parse(response);
-           res.redirect(`zarvich.netlify.app/viewBooking?status=${_results.STATUS}&ORDERID=${_results.ORDERID}&date=${_results.TXNDATE}&bank=${_results.BANKNAME}`)
+           res.redirect(`https://zarvich.netlify.app//viewBooking?status=${_results.STATUS}&ORDERID=${_results.ORDERID}&date=${_results.TXNDATE}&bank=${_results.BANKNAME}`)
            });
        });
 
